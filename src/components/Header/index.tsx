dca53ceda3345b21fa7e6fc62acccb165ebe01e1
chaerin-77 <rosachae12@gmail.com>
2024-06-25 17:23:56 +0900

    navbar 수정

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,36 +1,25 @@
-import { Routes, Route, Link } from 'react-router-dom';
-import { useContext } from 'react';
-import { UserContext } from 'contexts/Login';
+import { Routes, Route, Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "contexts/Login";
+import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faBell, faUser } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Header = () => {
-    const { userInfo } = useContext(UserContext);
-    const { logout } = useContext(UserContext);
+  const { userInfo } = useContext(UserContext);
+  const { logout } = useContext(UserContext);
 
-    return (
-        <nav className="flex justify-between">
-            <div className="flex">
-                <Link to="/">home</Link>
-                <Link to="/board">게시판</Link>
-                <Link to="/matching">매칭</Link>
-            </div>
-
-            <div className="flex">
-                {
-                    userInfo == null ? (
-                        <>
-                        <Link to="/login">로그인</Link>
-                        <Link to="/sign-up">회원가입</Link>
-                        </>
-                    ) :
-                    (
-                        <>
-                        <span>{userInfo.userId}님 안녕하세요</span>
-                        <Link to="/" onClick={logout}>로그아웃</Link>
-                        </>
-                    )
-                }
-                                
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav className="flex justify-between px-4 py-4 mt-2">
+      <FontAwesomeIcon icon={faBars} className="" />
+      <Link to="/">
+        <img alt="logo" src="logo.png" width="25%" />
+      </Link>
+      <div className="space-x-4">
+        <FontAwesomeIcon icon={faSearch} />
+        <FontAwesomeIcon icon={faBell} />
+        <FontAwesomeIcon icon={faUser} />
+      </div>
+    </nav>
+  );
+};
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,26 +1,54 @@
-import { Header } from "components/Header"
+import { Header } from "components/Header";
+import { Routes, Route, Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "contexts/Login";
 import { useEffect } from "react";
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 declare global {
-    interface Window {
-      kakao: any;
-    }
+  interface Window {
+    kakao: any;
+  }
 }
 export const Main = () => {
-    
-    useEffect(() => {
-        let container = document.getElementById(`map`); // 지도를 담을 영역의 DOM 레퍼런스
-        let options = {
-          center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도 중심 좌표
-          level: 3, // 지도의 레벨(확대, 축소 정도)
-        };
-    
-        let map = new window.kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
-      }, []);
-    return (
-        <div>
-            <Header />
-            <div id="map" style={{ width: "750px", height: "750px" }} />;
+  useEffect(() => {
+    let container = document.getElementById(`map`); // 지도를 담을 영역의 DOM 레퍼런스
+    let options = {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도 중심 좌표
+      level: 3, // 지도의 레벨(확대, 축소 정도)
+    };
+
+    let map = new window.kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
+  }, []);
+  const { userInfo } = useContext(UserContext);
+  const { logout } = useContext(UserContext);
+  return (
+    <div>
+      <Header />
+      {/* 임시 버튼 */}
+      <div className="flex">
+        <Link to="/">home</Link>
+        <Link to="/board">게시판</Link>
+        <Link to="/matching">매칭</Link>
+
+        <div className="flex">
+          {userInfo == null ? (
+            <>
+              <Link to="/login">로그인</Link>
+              <Link to="/sign-up">회원가입</Link>
+            </>
+          ) : (
+            <>
+              <span>{userInfo.userId}님 안녕하세요</span>
+              <Link to="/" onClick={logout}>
+                로그아웃
+              </Link>
+            </>
+          )}
         </div>
-    )
-}
+      </div>
+      <div id="map" style={{ width: "350px", height: "350px" }} />
+    </div>
+  );
+};
